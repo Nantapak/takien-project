80096accc25866bf6623d0012970287c0474e889
nantapak <nantapak1011@gmail.com>
2016-10-02 14:39:36 +0700

    login Ok

--- app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/LoginActivity.java
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/LoginActivity.java
@@ -3,15 +3,19 @@ package suwuttipoj.nantapak.takienfloatingmarket;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -49,46 +53,101 @@ public class LoginActivity extends AppCompatActivity {
 
         }
 
-
-
     }   //clickMyLogin
 
     private class SynMember extends AsyncTask<String, Void, String> {
 
-        //Ecplicit
+        //Explicit
         private Context context;
+        private boolean aBoolean = true;
+        private String[] columnStrings = new String[]{
+                "MEM_ID",
+                "USER_NAME",
+                "USER_PASSWORD",
+                "MEM_FIRSTNAME",
+                "MEM_LASTNAME",
+                "MEM_ADDRESS",
+                "MEM_TEL",
+                "MEM_EMAIL"};
+        private String[] loginStrings;
 
         public SynMember(Context context) {
             this.context = context;
         }
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(String... strings) {
 
             try {
 
                 OkHttpClient okHttpClient = new OkHttpClient();
                 Request.Builder builder = new Request.Builder();
-                Request request = builder.url(params[0]).build();
+                Request request = builder.url(strings[0]).build();
                 Response response = okHttpClient.newCall(request).execute();
                 return response.body().string();
 
             } catch (Exception e) {
-                Log.d("2octV1", "e doInBack ==>" + e.toString());
+                Log.d("2octV1", "e doInBack ==> " + e.toString());
                 return null;
             }
 
-        }   //doInBack
+        }   // doInBack
 
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
-            Log.d("2octV1", "JSON ==>" + s);
+            Log.d("2octV1", "JSON ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+
+                loginStrings = new String[columnStrings.length];
+
+                for (int i=0;i<jsonArray.length();i+=1) {
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    if (userString.equals(jsonObject.getString(columnStrings[1]))) {
+
+                        aBoolean = false;
+                        for (int i1=0;i1 < columnStrings.length;i1+=1) {
+
+                            loginStrings[i1] = jsonObject.getString(columnStrings[i1]);
+                            Log.d("2octV2", "loginString(" + i1 + ") = " + loginStrings[i1]);
+
+                        }   // for
+
+                    }   // if
+                }   // for
+
+                //Check User
+                if (aBoolean) {
+                    //User False
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "User ผิด",
+                            "ไม่มี " + userString + " ในฐานข้อมูล ของเรา");
+                } else if (!passwordString.equals(loginStrings[2])) {
+                    //Password False
+                    MyAlert myAlert = new MyAlert();
+                    myAlert.myDialog(context, "Password False",
+                            "กรุณาลองใหม่ Password ผิด");
+                } else {
+                    //Password True
+                    Toast.makeText(context, "Welcome " + loginStrings[3] + " " + loginStrings[4],
+                            Toast.LENGTH_SHORT).show();
+                }
+
+
+            } catch (Exception e) {
+                Log.d("2octV1", "e onPost ==> " + e.toString());
+            }
 
         }   // onPost
 
-    } // SynMember Class
+    }   // SynMember Class
+
 
 
     public void clickCancel(View view) {
