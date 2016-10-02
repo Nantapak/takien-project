ebeaf6f77ffe4320b0e2c55913828bf13f99d989
nantapak <nantapak1011@gmail.com>
2016-10-02 14:11:02 +0700

    ReadJson Finish

--- app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/LoginActivity.java
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/LoginActivity.java
@@ -1,17 +1,24 @@
 package suwuttipoj.nantapak.takienfloatingmarket;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
     //Explicit
     private EditText userEditText, passwordEditText;
     private String userString, passwordString;
+    private static final String urlJSON = "http://swiftcodingthai.com/ton/get_data.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +40,56 @@ public class LoginActivity extends AppCompatActivity {
 
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "Have Space",
-                    "Please Fill All Every Blak");
+                    "Please Fill All Every Blank");
 
         } else {
+
+            SynMember synMember = new SynMember(this);
+            synMember.execute(urlJSON);
 
         }
 
 
 
     }   //clickMyLogin
+
+    private class SynMember extends AsyncTask<String, Void, String> {
+
+        //Ecplicit
+        private Context context;
+
+        public SynMember(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(params[0]).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e) {
+                Log.d("2octV1", "e doInBack ==>" + e.toString());
+                return null;
+            }
+
+        }   //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("2octV1", "JSON ==>" + s);
+
+        }   // onPost
+
+    } // SynMember Class
+
 
     public void clickCancel(View view) {
         finish();
