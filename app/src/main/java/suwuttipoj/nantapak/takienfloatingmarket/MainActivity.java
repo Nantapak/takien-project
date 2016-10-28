0c54f747b62f3017a59fe57b4f74a4674f17f535
nantapak <nantapak1011@gmail.com>
2016-10-28 13:21:23 +0700

    Maps Ok

---
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/MainActivity.java
@@ -0,0 +1,27 @@
+package suwuttipoj.nantapak.takienfloatingmarket;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void clickMaps(View view) {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickMember(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+
+    }
+}
